925c4cdf1a8883214b07f22dc229a5bfc43fc2c6
Nota Name <email@thisisnotanemail.com>
2023-09-04 13:35:50 -0700

    enhance

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ let players = [
     { score: 0, coins: 3, powerups: [], heldBlackDice: null }
 ];
 let currentPlayer = 0;
+let currentDecisionFunction = null;
+
 function rollDice() {
     const roll = (sides) => Math.floor(Math.random() * sides) + 1;
     let redResult = roll(6);
@@ -19,32 +21,56 @@ function rollDice() {
     displayResults(redResult, yellowResult, blueResult, greenResult, blackResult);
     currentPlayer = (currentPlayer + 1) % 2;
 }
+
 function interpretRedDice(result, player) {
     switch (result) {
         case 1:
         case 2:
             player.score += 1;
-            if (player.coins >= 3 && confirm("Do you want to spend 3 coins to move an additional space?")) {
-                player.score += 1;
-                player.coins -= 3;
+            if (player.coins >= 3) {
+                askForDecision([
+                    { value: 'move', text: 'Move an additional space' },
+                    { value: 'stay', text: 'Stay' }
+                ], decision => {
+                    if (decision === 'move') {
+                        player.score += 1;
+                        player.coins -= 3;
+                    }
+                    updateStats();
+                });
             }
             break;
         case 3:
         case 4:
             player.score += 2;
-            if (player.coins >= 2 && confirm("Spend 2 coins to prevent an opponent from moving next turn?")) {
-                player.coins -= 2;
+            if (player.coins >= 2) {
+                askForDecision([
+                    { value: 'spend', text: 'Spend 2 coins to prevent an opponent from moving next turn' },
+                    { value: 'stay', text: 'Stay' }
+                ], decision => {
+                    if (decision === 'spend') {
+                        player.coins -= 2;
+                    }
+                    updateStats();
+                });
             }
             break;
         case 5:
-            let chosenPowerUp = prompt("Choose a power-up: Mushroom, Fire Flower, Feather", "Mushroom");
-            player.powerups.push(chosenPowerUp);
+            askForDecision([
+                { value: 'Mushroom', text: 'Mushroom' },
+                { value: 'Fire Flower', text: 'Fire Flower' },
+                { value: 'Feather', text: 'Feather' }
+            ], decision => {
+                player.powerups.push(decision);
+                updateStats();
+            });
             break;
         case 6:
             player.coins += roll(6);
             break;
     }
 }
+
 function interpretYellowDice(result, player) {
     if (result >= 1 && result <= 4) {
         player.coins += result;
@@ -52,56 +78,88 @@ function interpretYellowDice(result, player) {
         player.coins += 3;
     }
 }
+
 function interpretBlueDice(result, player) {
     switch (result) {
         case 4:
         case 5:
-            if (player.coins >= 2 && confirm("Strong Current detected! Do you want to spend 2 coins to protect yourself?")) {
-                player.coins -= 2;
-            } else {
-                player.score -= 1;
+            if (player.coins >= 2) {
+                askForDecision([
+                    { value: 'spend', text: 'Spend 2 coins to protect yourself from the Strong Current' },
+                    { value: 'stay', text: 'Stay' }
+                ], decision => {
+                    if (decision === 'spend') {
+                        player.coins -= 2;
+                    } else {
+                        player.score -= 1;
+                    }
+                    updateStats();
+                });
             }
             break;
         case 6:
-            if (confirm("You got a Blue Shell! Do you want to push yourself back by 2 spaces?")) {
-                player.score -= 2;
-            }
+            askForDecision([
+                { value: 'push', text: 'Push yourself back by 2 spaces' },
+                { value: 'stay', text: 'Stay' }
+            ], decision => {
+                if (decision === 'push') {
+                    player.score -= 2;
+                }
+                updateStats();
+            });
             break;
     }
 }
+
 function interpretGreenDice(result, player) {
     switch (result) {
         case 1:
         case 2:
-            let decision = prompt("You found a Short Pipe! Do you want to 'move' forward by 1 space or 'collect' 3 coins?", "move");
-            if (decision === "move") {
-                player.score += 1;
-            } else {
-                player.coins += 3;
-            }
+            askForDecision([
+                { value: 'move', text: 'Move forward by 1 space' },
+                { value: 'collect', text: 'Collect 3 coins' }
+            ], decision => {
+                if (decision === 'move') {
+                    player.score += 1;
+                } else {
+                    player.coins += 3;
+                }
+                updateStats();
+            });
             break;
         case 3:
         case 4:
-            let mediumChoice = prompt("Medium pipe: Choose to skip your next turn and then move 3 spaces or gain a power-up.", "move");
-            if (mediumChoice === "move") {
-                player.score += 3;
-            } else {
-                player.powerups.push("Choose a power-up");
-            }
+            askForDecision([
+                { value: 'move', text: 'Skip your next turn and then move 3 spaces' },
+                { value: 'collect', text: 'Gain a power-up' }
+            ], decision => {
+                if (decision === 'move') {
+                    player.score += 3;
+                } else {
+                    player.powerups.push('Choose a power-up');
+                }
+                updateStats();
+            });
             break;
         case 5:
-            let longChoice = prompt("Long pipe: Choose to skip 2 turns and then move 5 spaces or gain 2 power-ups.", "move");
-            if (longChoice === "move") {
-                player.score += 5;
-            } else {
-                player.powerups.push("Choose a power-up", "Choose another power-up");
-            }
+            askForDecision([
+                { value: 'move', text: 'Skip 2 turns and then move 5 spaces' },
+                { value: 'collect', text: 'Gain 2 power-ups' }
+            ], decision => {
+                if (decision === 'move') {
+                    player.score += 5;
+                } else {
+                    player.powerups.push('Choose a power-up', 'Choose another power-up');
+                }
+                updateStats();
+            });
             break;
         case 6:
             player.score = 0;
             break;
     }
 }
+
 function interpretBlackDice(result, player) {
     switch (result) {
         case 1:
@@ -113,19 +171,30 @@ function interpretBlackDice(result, player) {
             break;
         case 4:
         case 5:
-            let decision = confirm("Bowser's Minion spotted! Do you want to move back by 1 space?");
-            if (decision) {
-                player.score -= 1;
-            }
+            askForDecision([
+                { value: 'move', text: 'Move back by 1 space' },
+                { value: 'stay', text: 'Stay' }
+            ], decision => {
+                if (decision === 'move') {
+                    player.score -= 1;
+                }
+                updateStats();
+            });
             break;
         case 6:
-            let wrathDecision = confirm("Bowser's Wrath! Do you want to lose half your coins?");
-            if (wrathDecision) {
-                player.coins = Math.floor(player.coins / 2);
-            }
+            askForDecision([
+                { value: 'lose', text: 'Lose half your coins' },
+                { value: 'stay', text: 'Stay' }
+            ], decision => {
+                if (decision === 'lose') {
+                    player.coins = Math.floor(player.coins / 2);
+                }
+                updateStats();
+            });
             break;
     }
 }
+
 function updateStats() {
     document.getElementById('score1').textContent = players[0].score;
     document.getElementById('coins1').textContent = players[0].coins;
@@ -135,6 +204,7 @@ function updateStats() {
     document.getElementById('powerups2').textContent = players[1].powerups.join(", ") || "None";
     updateBoardPosition();
 }
+
 function updateBoardPosition() {
     const terrains = ["🌲", "🏞️", "🏖️", "🌋", "🏔️", "🏜️", "🏙️", "🌉", "🚀", "🌌", "🌕", "🌠", "🪐", "🌌", "🚀", "🌉", "🏙️", "🏜️", "🎡", "🏰"];
     for (let i = 0; i < 20; i++) {
@@ -147,6 +217,7 @@ function updateBoardPosition() {
         document.getElementById('space' + players[1].score).textContent += "🔵";
     }
 }
+
 function displayResults(red, yellow, blue, green, black) {
     document.getElementById('results').innerHTML = `
         Red (Mario): ${red} <br>
@@ -155,4 +226,26 @@ function displayResults(red, yellow, blue, green, black) {
         Green (Pipes): ${green} <br>
         Black (Bowser): ${black}
     `;
+}
+
+function askForDecision(options, callback) {
+    currentDecisionFunction = callback;
+    const decisionSelect = document.getElementById('decisionOptions');
+    decisionSelect.innerHTML = '';
+    options.forEach(option => {
+        const opt = document.createElement('option');
+        opt.value = option.value;
+        opt.innerText = option.text;
+        decisionSelect.appendChild(opt);
+    });
+    document.getElementById('decisionBox').style.display = 'block';
+}
+
+function confirmDecision() {
+    const selectedValue = document.getElementById('decisionOptions').value;
+    if (currentDecisionFunction) {
+        currentDecisionFunction(selectedValue);
+        currentDecisionFunction = null;
+    }
+    document.getElementById('decisionBox').style.display = 'none';
 }
